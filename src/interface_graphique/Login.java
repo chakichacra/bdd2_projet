51070c6cef2d965b2f59dce8be494811ec17e324
Your Name <sdkjfh>
2020-05-30 12:44:48 +0200

    design page login presque finis

--- src/interface_graphique/Login.java
+++ src/interface_graphique/Login.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -36,26 +37,38 @@ public class Login {
 		
 		//Creation de la ligne identifiant et ajout au panel formulaire		
 		JTextField loginInput = new JTextField();
-		loginInput.setPreferredSize(new Dimension(100, 20));		
+		loginInput.setPreferredSize(new Dimension(200, 30));
+		loginInput.setFont(loginInput.getFont().deriveFont(30.0f));
 		JPanel loginLine = new JPanel();
 		JLabel labelIdentifiant = new JLabel();
 		labelIdentifiant.setText("Identifiant: ");
+		labelIdentifiant.setFont(titre.getFont().deriveFont(30.0f));
+		labelIdentifiant.setForeground(Color.white);
 		loginLine.add(labelIdentifiant);
-		loginLine.add(loginInput);		
+		loginLine.add(loginInput);
+		loginLine.setBackground(Color.darkGray);
 		this.formulaire.add(loginLine, BorderLayout.NORTH);
 		
 		//Creation de la ligne mot de passe et ajout au panel formulaire
 		JTextField passwordInput = new JTextField();
-		passwordInput.setPreferredSize(new Dimension(100, 20));		
+		passwordInput.setPreferredSize(new Dimension(200, 30));	
+		passwordInput.setFont(passwordInput.getFont().deriveFont(30.0f));
 		JPanel passwordLine = new JPanel();
 		JLabel labelPassword = new JLabel();
 		labelPassword.setText("Mot de passe: ");
+		labelPassword.setFont(titre.getFont().deriveFont(30.0f));
+		labelPassword.setForeground(Color.white);
 		passwordLine.add(labelPassword);
-		passwordLine.add(passwordInput);		
+		passwordLine.add(passwordInput);
+		passwordLine.setBackground(Color.darkGray);
 		this.formulaire.add(passwordLine);
+		
+		//Creation du bouton creer un compte
+		JButton boutonCreeCompte = new JButton("Créer un compte");
 		
 		this.pan.setLayout(new BoxLayout(this.pan, BoxLayout.PAGE_AXIS));
 		this.pan.add(this.formulaire, BorderLayout.CENTER);
+		this.pan.add(boutonCreeCompte, BorderLayout.PAGE_END);
 		this.formulaire.setBackground(Color.darkGray);
 	}
 	
