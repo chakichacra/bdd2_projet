3da8ddb1b8edd92fc3f8856e0a63bef4b29dc98b
EduinAGUIRREOSORIO <EduinAGUIRREOSORIO@DESKTOP-HT1T1TH>
2020-05-30 23:49:35 +0200

    consultationRDV/entete

--- src/interface_graphique/consultationRDV.java
+++ src/interface_graphique/consultationRDV.java
@@ -1,13 +1,49 @@
 package interface_graphique;
 
 import javax.swing.JPanel;
+import java.awt.Point;
+import java.awt.Dimension;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class consultationRDV extends JPanel {
+	private JTextField txtConsultationsDesRendezvous;
 
 	/**
 	 * Create the panel.
 	 */
 	public consultationRDV() {
+		setPreferredSize(new Dimension(800, 865));
+		setLocation(new Point(200, 135));
+		setLayout(null);
+		
+		txtConsultationsDesRendezvous = new JTextField();
+		txtConsultationsDesRendezvous.setFont(new Font("Verdana", Font.BOLD, 16));
+		txtConsultationsDesRendezvous.setHorizontalAlignment(SwingConstants.CENTER);
+		txtConsultationsDesRendezvous.setText("Consultations des rendez-vous");
+		txtConsultationsDesRendezvous.setBounds(10, 11, 780, 70);
+		add(txtConsultationsDesRendezvous);
+		txtConsultationsDesRendezvous.setColumns(10);
+		
+		JButton btnNewButton = new JButton("par semaine");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(160, 155, 159, 40);
+		add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("pour un jour");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(460, 155, 165, 40);
+		add(btnNewButton_1);
 
 	}
 
--- src/interface_graphique/entete.java
+++ src/interface_graphique/entete.java
@@ -1,6 +1,13 @@
 package interface_graphique;
 
 import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.SwingConstants;
 
 public class entete extends JPanel {
 
@@ -8,7 +15,20 @@ public class entete extends JPanel {
 	 * Create the panel.
 	 */
 	public entete() {
+		setPreferredSize(new Dimension(1000, 1000));
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		panel.setBounds(0, 0, 1000, 135);
+		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Application de gestion de rendez-vous pour psychologue");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 22));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(102, 32, 735, 51);
+		panel.add(lblNewLabel);
 
 	}
-
 }
