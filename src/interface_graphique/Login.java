bed7500d893ca84de12f852809cf3c3a45d2099f
Your Name <sdkjfh>
2020-05-30 12:21:45 +0200

    first

---
+++ src/interface_graphique/Login.java
@@ -0,0 +1,65 @@
+package interface_graphique;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class Login {
+	
+	private JPanel pan;
+	private JPanel formulaire;
+	
+	public Login() {
+		//set up du panel principal
+		this.pan = new JPanel();
+		this.pan.setBackground(Color.darkGray);
+		this.pan.setLayout(new BorderLayout());
+		
+		//Creation du panel qui comportera les input login/password		
+		this.formulaire = new JPanel();
+		this.formulaire.setBackground(Color.darkGray);
+		this.formulaire.setLayout(new BorderLayout());
+		this.formulaire.setBorder(new EmptyBorder(200, 10, 100, 10));
+		
+		//Creation du titre de la fenetre Connexion
+		JLabel titre = new JLabel();
+		titre.setFont(titre.getFont().deriveFont(50.0f));
+		titre.setText("Connexion");
+		titre.setForeground(Color.white);
+		pan.add(titre);
+		
+		//Creation de la ligne identifiant et ajout au panel formulaire		
+		JTextField loginInput = new JTextField();
+		loginInput.setPreferredSize(new Dimension(100, 20));		
+		JPanel loginLine = new JPanel();
+		JLabel labelIdentifiant = new JLabel();
+		labelIdentifiant.setText("Identifiant: ");
+		loginLine.add(labelIdentifiant);
+		loginLine.add(loginInput);		
+		this.formulaire.add(loginLine, BorderLayout.NORTH);
+		
+		//Creation de la ligne mot de passe et ajout au panel formulaire
+		JTextField passwordInput = new JTextField();
+		passwordInput.setPreferredSize(new Dimension(100, 20));		
+		JPanel passwordLine = new JPanel();
+		JLabel labelPassword = new JLabel();
+		labelPassword.setText("Mot de passe: ");
+		passwordLine.add(labelPassword);
+		passwordLine.add(passwordInput);		
+		this.formulaire.add(passwordLine);
+		
+		this.pan.setLayout(new BoxLayout(this.pan, BoxLayout.PAGE_AXIS));
+		this.pan.add(this.formulaire, BorderLayout.CENTER);
+		this.formulaire.setBackground(Color.darkGray);
+	}
+	
+	public JPanel getJPanel() {
+		return this.pan;
+	}
+}
---
+++ src/utilitaire/Utilitaire.java
@@ -0,0 +1,61 @@
+package utilitaire;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.math.BigInteger; 
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException; 
+
+public class Utilitaire {
+	
+	public static Connection loadDatabase() {
+		Connection connexion;
+		try {
+	        Class.forName("com.mysql.cj.jdbc.Driver");
+	    } catch (ClassNotFoundException e) {
+	    }
+
+	    try {
+	        connexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/gdc?" + 
+	        		"serverTimezone=UTC", "root", "efrei123");
+	    } 
+		catch (SQLException e) {
+			connexion = null;
+	    }
+	    return connexion;
+	}
+	
+	public static String encryptThisString(String input) 
+    { 
+        try { 
+            // getInstance() method is called with algorithm SHA-1 
+            MessageDigest md = MessageDigest.getInstance("SHA-1"); 
+  
+            // digest() method is called 
+            // to calculate message digest of the input string 
+            // returned as array of byte 
+            byte[] messageDigest = md.digest(input.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+  
+            // Add preceding 0s to make it 32 bit 
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            } 
+  
+            // return the HashText 
+            return hashtext; 
+        } 
+  
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            throw new RuntimeException(e); 
+        } 
+    } 
+
+}
